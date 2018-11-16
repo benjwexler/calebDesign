d39244a47664cca4a3b9021891f99f56d5772a6e
Ben Wexler <mixedywex@gmail.com>
2018-11-16 01:54:15 -0400

    soundcloud player working

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // function latestReleasesScroll() {
 //     window.scrollBy(0, 840);
- 
+
 
 // }
 
@@ -13,83 +13,239 @@ console.log("hey")
 
 function scrollIt(destination, duration = 200, easing = 'linear', callback) {
 
-    const easings = {
-      linear(t) {
-        return t;
-      },
-      easeInQuad(t) {
-        return t * t;
-      },
-      easeOutQuad(t) {
-        return t * (2 - t);
-      },
-      easeInOutQuad(t) {
-        return t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t;
-      },
-      easeInCubic(t) {
-        return t * t * t;
-      },
-      easeOutCubic(t) {
-        return (--t) * t * t + 1;
-      },
-      easeInOutCubic(t) {
-        return t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1;
-      },
-      easeInQuart(t) {
-        return t * t * t * t;
-      },
-      easeOutQuart(t) {
-        return 1 - (--t) * t * t * t;
-      },
-      easeInOutQuart(t) {
-        return t < 0.5 ? 8 * t * t * t * t : 1 - 8 * (--t) * t * t * t;
-      },
-      easeInQuint(t) {
-        return t * t * t * t * t;
-      },
-      easeOutQuint(t) {
-        return 1 + (--t) * t * t * t * t;
-      },
-      easeInOutQuint(t) {
-        return t < 0.5 ? 16 * t * t * t * t * t : 1 + 16 * (--t) * t * t * t * t;
-      }
-    };
-  
-    const start = window.pageYOffset;
-    const startTime = 'now' in window.performance ? performance.now() : new Date().getTime();
-  
-    const documentHeight = Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);
-    const windowHeight = window.innerHeight || document.documentElement.clientHeight || document.getElementsByTagName('body')[0].clientHeight;
-    const destinationOffset = typeof destination === 'number' ? destination : destination.offsetTop;
-    const destinationOffsetToScroll = Math.round(documentHeight - destinationOffset < windowHeight ? documentHeight - windowHeight : destinationOffset);
-  
-    if ('requestAnimationFrame' in window === false) {
-      window.scroll(0, destinationOffsetToScroll);
+  const easings = {
+    linear(t) {
+      return t;
+    },
+    easeInQuad(t) {
+      return t * t;
+    },
+    easeOutQuad(t) {
+      return t * (2 - t);
+    },
+    easeInOutQuad(t) {
+      return t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t;
+    },
+    easeInCubic(t) {
+      return t * t * t;
+    },
+    easeOutCubic(t) {
+      return (--t) * t * t + 1;
+    },
+    easeInOutCubic(t) {
+      return t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1;
+    },
+    easeInQuart(t) {
+      return t * t * t * t;
+    },
+    easeOutQuart(t) {
+      return 1 - (--t) * t * t * t;
+    },
+    easeInOutQuart(t) {
+      return t < 0.5 ? 8 * t * t * t * t : 1 - 8 * (--t) * t * t * t;
+    },
+    easeInQuint(t) {
+      return t * t * t * t * t;
+    },
+    easeOutQuint(t) {
+      return 1 + (--t) * t * t * t * t;
+    },
+    easeInOutQuint(t) {
+      return t < 0.5 ? 16 * t * t * t * t * t : 1 + 16 * (--t) * t * t * t * t;
+    }
+  };
+
+  const start = window.pageYOffset;
+  const startTime = 'now' in window.performance ? performance.now() : new Date().getTime();
+
+  const documentHeight = Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);
+  const windowHeight = window.innerHeight || document.documentElement.clientHeight || document.getElementsByTagName('body')[0].clientHeight;
+  const destinationOffset = typeof destination === 'number' ? destination : destination.offsetTop;
+  const destinationOffsetToScroll = Math.round(documentHeight - destinationOffset < windowHeight ? documentHeight - windowHeight : destinationOffset);
+
+  if ('requestAnimationFrame' in window === false) {
+    window.scroll(0, destinationOffsetToScroll);
+    if (callback) {
+      callback();
+    }
+    return;
+  }
+
+  function scroll() {
+    const now = 'now' in window.performance ? performance.now() : new Date().getTime();
+    const time = Math.min(1, ((now - startTime) / duration));
+    const timeFunction = easings[easing](time);
+    window.scroll(0, Math.ceil((timeFunction * (destinationOffsetToScroll - start)) + start));
+
+    if (window.pageYOffset === destinationOffsetToScroll) {
       if (callback) {
         callback();
       }
       return;
     }
-  
-    function scroll() {
-      const now = 'now' in window.performance ? performance.now() : new Date().getTime();
-      const time = Math.min(1, ((now - startTime) / duration));
-      const timeFunction = easings[easing](time);
-      window.scroll(0, Math.ceil((timeFunction * (destinationOffsetToScroll - start)) + start));
-  
-      if (window.pageYOffset === destinationOffsetToScroll) {
-        if (callback) {
-          callback();
-        }
-        return;
-      }
-  
-      requestAnimationFrame(scroll);
-    }
-  
-    scroll();
+
+    requestAnimationFrame(scroll);
   }
+
+  scroll();
+}
 
 document.querySelector('#latestReleasesLink').addEventListener('click', () => scrollIt(document.getElementById("section2")));
 
 document.querySelector('#drumMachineLink').addEventListener('click', () => scrollIt(document.getElementById("drumMachineSection")));
+
+
+
+var widgetIframe = document.getElementById('sc-widget'),
+  widget1 = SC.Widget(widgetIframe);
+
+widget1.bind(SC.Widget.Events.READY, function () {
+  console.log("Yjefnifr")
+
+  widget1.getVolume(function (volume) {
+    console.log('current volume value is ' + volume);
+  });
+
+  widget1.setVolume(100);
+
+
+});
+
+var widgetIframe2 = document.getElementById('sc-widget2'),
+  widget2 = SC.Widget(widgetIframe2);
+
+widget2.bind(SC.Widget.Events.READY, function () {
+  console.log("Yjefnifr")
+
+  widget2.getVolume(function (volume) {
+    console.log('current volume value is ' + volume);
+  });
+
+  widget2.setVolume(100);
+
+
+});
+
+var widgetIframe3 = document.getElementById('sc-widget3'),
+  widget3 = SC.Widget(widgetIframe3);
+
+widget3.bind(SC.Widget.Events.READY, function () {
+  console.log("Yjefnifr")
+
+  widget3.getVolume(function (volume) {
+    console.log('current volume value is ' + volume);
+  });
+
+  widget3.setVolume(100);
+
+
+});
+
+var widgetIframe4 = document.getElementById('sc-widget4'),
+  widget4 = SC.Widget(widgetIframe4);
+
+widget4.bind(SC.Widget.Events.READY, function () {
+  console.log("Yjefnifr")
+
+  widget4.getVolume(function (volume) {
+    console.log('current volume value is ' + volume);
+  });
+
+  widget4.setVolume(100);
+
+
+});
+
+
+
+// let track1counter = 0
+// let track2counter = 0
+
+let currentTrack
+
+function playSound() {
+
+  console.log(this.id)
+
+
+
+var trackNumber = this.id;
+var numberPattern = /\d+/g;
+trackNumber = trackNumber.match( numberPattern ).join([]);
+trackNumber = parseInt(trackNumber)
+
+if (currentTrack === undefined) {
+  currentTrack = trackNumber
+} 
+
+
+// alert(trackNumber);
+  
+  // document.getElementById("sc-widget").src = "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/522685110&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+  // widget.seekTo(0)
+  if(window[`track${trackNumber}counter`] === 0) {
+    
+    window[`widget${trackNumber}`].play();
+    document.getElementById(`playAndPauseIcon${trackNumber}`).classList.remove("fa-play");
+    document.getElementById(`playAndPauseIcon${trackNumber}`).classList.add("fa-pause")
+    if(currentTrack !== trackNumber) {
+      document.getElementById(`playAndPauseIcon${currentTrack}`).classList.remove("fa-pause");
+      document.getElementById(`playAndPauseIcon${currentTrack}`).classList.add("fa-play")
+      window[`track${currentTrack}counter`] = 0
+      window[`widget${trackNumber}`].seekTo(0)
+    }
+    
+    window[`track${trackNumber}counter`] = 1
+  }
+
+  else {
+    console.log("else")
+    window[`widget${trackNumber}`].pause()
+    
+    document.getElementById(`playAndPauseIcon${trackNumber}`).classList.remove("fa-pause");
+    document.getElementById(`playAndPauseIcon${trackNumber}`).classList.add("fa-play")
+    window[`track${trackNumber}counter`] = 0
+
+  }
+  currentTrack = trackNumber
+}
+
+// function playSound2() {
+  
+//   // document.getElementById("sc-widget").src = "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/522685110&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+  
+//   if(track2counter === 0) {
+
+    
+
+//     widget2.play()
+//     document.getElementById("playAndPauseIcon2").classList.remove("fa-play")
+//     document.getElementById("playAndPauseIcon2").classList.add("fa-pause")
+//     track2counter = 1
+//   } else {
+//     widget2.pause()
+//     document.getElementById("playAndPauseIcon2").classList.remove("fa-pause")
+//     document.getElementById("playAndPauseIcon2").classList.add("fa-play")
+//     track2counter = 0
+//   }
+
+// }
+// document.getElementById("playAndPauseIcon1").addEventListener("click", playSound)
+// document.getElementById("playAndPauseIcon2").addEventListener("click", playSound2)
+//  document.getElementById("soundcloud").addEventListener("load", playSound);
+
+let tracks = document.getElementsByClassName("playAndPauseIcon")
+
+for (let i = 0; i < tracks.length; i++) {
+  window[`track${i + 1}`] = tracks[i]
+  window[`track${i + 1}counter`] = 0
+  
+  window[`track${i + 1}`].addEventListener("click", playSound)
+
+}
+
+
+
+
+
+
